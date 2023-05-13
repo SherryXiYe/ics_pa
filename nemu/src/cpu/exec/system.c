4b5cecb78e275fb9f66e01e261119ed7a17286fb
tracer-ics2017 <tracer@njuics.org>
2023-05-13 21:33:24 +0800

    >  compile 2010507 zihan Linux ubuntu 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:33:24 up  2:50,  1 user,  load average: 0.04, 0.06, 0.02 902889cfcf98cd944a38aa3ee72e3a3745e66c55

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -42,8 +42,11 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  TODO();
-
+  // TODO();
+  rtl_pop(&decoding.jmp_eip);
+  decoding.is_jmp = 1;
+  rtl_pop(&cpu.CS);
+  rtl_pop(&cpu.eflags);
   print_asm("iret");
 }
 
