c8b410446df3e295537c92e4782ac95293faff0a
tracer-ics2017 <tracer@njuics.org>
2023-05-13 14:57:35 +0800

    >  run 2010507 zihan Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:57:35 up  1:56,  1 user,  load average: 0.38, 0.22, 0.19 631b1c0c95dbad2def2a726bbccce55f945521ae

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,13 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern size_t get_ramdisk_size();
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  // TODO();
+  size_t ramdisk_size = get_ramdisk_size();
+  ramdisk_read((void *)DEFAULT_ENTRY, 0, ramdisk_size);
   return (uintptr_t)DEFAULT_ENTRY;
 }
