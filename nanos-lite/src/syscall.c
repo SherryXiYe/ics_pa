6292df462292978506343f4e33061b5aeac64ec3
tracer-ics2017 <tracer@njuics.org>
2023-05-15 17:32:58 +0800

    >  run 2010507 zihan Linux ubuntu 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:32:58 up  4:06,  1 user,  load average: 0.49, 0.27, 0.13 38328d25d0536bbd737a314cb2801294993dc9

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,7 +11,7 @@ void sys_exit(int n){
 
 int sys_write(int fd,void *buf,size_t len){
   if(fd==1||fd==2){
-    Log("sys_write buf: %s",(char*)buf);
+    // Log("sys_write buf: %s",(char*)buf);
     char c;
     for(int i=0;i<len;++i){
       memcpy(&c,buf+i,1);
