958cc6b49da97861983d388056b93184155faacb
tracer-ics2017 <tracer@njuics.org>
2023-05-15 15:19:31 +0800

    >  run 2010507 zihan Linux ubuntu 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:19:31 up  1:53,  1 user,  load average: 0.14, 0.12, 0.06 fda5d960404814f91f683ac69ed92c276a79e820

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,9 +1,14 @@
 #include "fs.h"
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern void ramdisk_write(const void *buf, off_t offset, size_t len);
+
+// 文件记录表
 typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;  //偏移量属性open_offset,来记录目前文件操作的位置
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -23,4 +28,44 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+// 返回文件描述符fd所描述的文件的大小
+size_t fs_filesz(int fd) {
+  return file_table[fd].size;
+}
+
+int fs_open(const char *pathname, int flags, int mode) {
+  for(int i = 0; i < NR_FILES; i++)
+    if(strcmp(file_table[i].name, pathname) == 0)
+      return i;
+  panic("The file indicated by pathname was not found.\n");
+  return -1;
+}
+
+ssize_t fs_read(int fd, void *buf, size_t len) {
+  assert(fd>=0&&fd<NR_FILES);
+  if(fd==FD_STDIN||fd==FD_STDOUT||fd==FD_STDERR){
+    Log("Invalid fd.\n");
+    return 0;
+  }
+  ssize_t size = file_table[fd].size;
+  if(file_table[fd].open_offset + len > size)
+    len = size - file_table[fd].open_offset;
+  ramdisk_read(buf, file_table[fd].open_offset + file_table[fd].disk_offset, len);
+  file_table[fd].open_offset += len;
+  return len;
+}
+
+int fs_close(int fd) {
+  assert(fd>=0&&fd<NR_FILES);
+  return 0;
+}
+
+ssize_t fs_write(int fd, const void *buf, size_t len){
+  return 0;
+}
+
+off_t fs_lseek(int fd, off_t offset, int whence){
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include"fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
@@ -8,7 +9,10 @@ extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  size_t ramdisk_size = get_ramdisk_size();
-  ramdisk_read((void *)DEFAULT_ENTRY, 0, ramdisk_size);
+  // size_t ramdisk_size = get_ramdisk_size();
+  // ramdisk_read((void *)DEFAULT_ENTRY, 0, ramdisk_size);
+  int fd = fs_open(filename, 0, 0);
+  fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));  //把整个filename文件读入DEFAULT_ENTRY处
+  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
