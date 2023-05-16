037c368f6fa4b3c84c4446d8e3cbd06dfb2e83ab
tracer-ics2017 <tracer@njuics.org>
2023-05-16 16:36:26 +0800

    >  run 2010507 zihan Linux ubuntu 5.15.0-71-generic #78~20.04.1-Ubuntu SMP Wed Apr 19 11:26:48 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:36:26 up 47 min,  1 user,  load average: 0.16, 0.54, 0.60 cd2551585df12ea0ef3086d66546abaf753cd8a9

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -9,26 +9,17 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t len) {
-  char str[20];
-  bool down=false;
-  int key=_read_key();
-  if(key&0x8000){
-    key^=0x8000;
-    down=true;
+  int key = _read_key();
+  bool down = false;
+  if (key & 0x8000) {
+    key &= 0xfff;
+    down = true;
   }
-  if(key!=_KEY_NONE){
-    Log("in");
-    sprintf(str,"%s %s\n",down?"kd":"ku",keyname[key]);
-  }else{
-    sprintf(str,"t %d\n",_uptime());
-  }
-  Log("len: %d; str: %s , %d",len,str,strlen(str));
-  if(strlen(str)<=len){
-    strncpy((char*)buf,str,strlen(str));
-    return strlen(str);
-  }
-  Log("error:strlen(str)>len");
-  return 0;
+  if (key) 
+    sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
+  else
+    sprintf(buf, "t %d\n", (uint32_t)_uptime());
+  return strlen(buf);
 }
 
 static char dispinfo[128] __attribute__((used));
