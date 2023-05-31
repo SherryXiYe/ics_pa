9c9deefafba72e2ea1687b805de1c094a301dba0
tracer-ics2017 <tracer@njuics.org>
2023-05-31 16:25:34 +0800

    >  compile 2010507 zihan Linux ubuntu 5.15.0-72-generic #79~20.04.1-Ubuntu SMP Thu Apr 20 22:12:07 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:25:34 up  1:28,  1 user,  load average: 0.05, 0.09, 0.04 f715ca65fa9a0c679444bbb06fc77d274db14e9f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -10,6 +10,18 @@
 
 uint8_t pmem[PMEM_SIZE];      // 模拟内存
 
+// +--------10------+-------10-------+---------12----------+
+// | Page Directory |   Page Table   | Offset within Page  |
+// |      Index     |      Index     |                     |
+// +----------------+----------------+---------------------+
+//  \--- PDX(va) --/ \--- PTX(va) --/\------ OFF(va) ------/
+#define PDX(va)     (((uint32_t)(va) >> 22) & 0x3ff)
+#define PTX(va)     (((uint32_t)(va) >> 12) & 0x3ff)
+#define OFF(va)     ((uint32_t)(va) & 0xfff)
+
+// Address in page table or page directory entry 获取高20位
+#define PTE_ADDR(pte)   ((uint32_t)(pte) & ~0xfff)
+
 /* Memory accessing interfaces */
 
 // len表示取从低位开始的多少个字节，len一般取1~4。
@@ -30,10 +42,48 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
     mmio_write(addr, len, data, no);
 }
 
+paddr_t page_translate(vaddr_t addr, bool dirty) {
+  if(cpu.cr0.paging && cpu.cr0.protect_enable){   
+    PDE* pgdir = (PDE*)PTE_ADDR(cpu.cr3.val);   // 页目录表基址
+    PDE pde;
+    pde.val = paddr_read((paddr_t)(pgdir+PDX(addr)), 4);  //页目录项
+    assert(pde.present);
+    pde.accessed = true;
+
+    PTE* ptep = (PTE*)PTE_ADDR(pde.val);    // 二级页表基址
+    PTE pte;
+    pte.val = paddr_read((paddr_t)(ptep+PTX(addr)), 4);   //页表项
+    assert(pte.present);
+    pte.accessed = true;
+    pte.dirty = dirty;    // 设置dirty位
+
+    paddr_t paddr = PTE_ADDR(pte.val) | OFF(addr);
+    return paddr;
+  }
+  return addr;    // (cpu.cr0.val >> 31 != 1)
+}
+
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  // return paddr_read(addr, len);
+  uint32_t res = 0;
+  if (PTE_ADDR(addr) != PTE_ADDR(addr + len - 1)){    // 跨页，依次读取
+    for (int i = 0; i < len; i++) {
+      paddr_t paddr = page_translate(addr + i, false);
+      res |= paddr_read(paddr, 1) << (8 * i); 
+    }
+  }else{      // 不跨越，直接通过page_translate获取物理地址再读取
+    paddr_t paddr = page_translate(addr, false);
+    res = paddr_read(paddr, len);
+  }
+  return res;
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  // paddr_write(addr, len, data);
+  if (PTE_ADDR(addr) != PTE_ADDR(addr + len - 1)){    // 跨页，先退出
+    assert(0);
+  }
+  paddr_t paddr = page_translate(addr, true);
+  paddr_write(paddr, len, data);
 }
+
