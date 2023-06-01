c597a5b9fd6f33011ab0c39354aaf47b955d48aa
tracer-ics2017 <tracer@njuics.org>
2023-06-01 14:48:33 +0800

    >  run 2010507 zihan Linux ubuntu 5.15.0-72-generic #79~20.04.1-Ubuntu SMP Thu Apr 20 22:12:07 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:48:33 up 11:47,  1 user,  load average: 0.27, 0.16, 0.17 99aba9468df9094ee40858d51ef7b1b73d69b1fd

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,12 +26,24 @@ void load_prog(const char *filename) {
   pcb[i].tf = _umake(&pcb[i].as, stack, stack, (void *)entry, NULL, NULL);
 }
 
+static int count=0;
+static const int freq=1000;
+
 _RegSet* schedule(_RegSet *prev) {
   // return NULL;
   if(current!=NULL){      //current:当前进程的PCB指针
     current->tf=prev;     //保存tf
+  }else{
+    current=&pcb[0];      //初始进程为0号进程
   }
-  current=&pcb[0];          //切换到第一个用户进程
+  // current=(current==&pcb[0]?&pcb[1]:&pcb[0]);
+  count++;
+  if(count==freq){      //如果到达freq次，则切换成进程1
+    current=&pcb[1];
+    count=0;
+  }else{
+    current=&pcb[0];
+  }
   _switch(&current->as);    //切换虚拟地址空间
   return current->tf;
 }
