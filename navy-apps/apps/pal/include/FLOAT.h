6935809aee5a75fbed96b836ba957993b3d44db5
tracer-ics2017 <tracer@njuics.org>
2023-06-14 03:32:58 +0800

    >  compile 2010507 zihan Linux ubuntu 5.15.0-73-generic #80~20.04.1-Ubuntu SMP Wed May 17 14:58:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:32:58 up 13:18,  1 user,  load average: 0.84, 0.39, 0.26 af331b26d7eb562d65aeb6517ca6232d72f13a

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -2,39 +2,38 @@
 #define __FLOAT_H__
 
 #include "assert.h"
-#include "common.h"
+
 typedef int FLOAT;
 
+#define isNeg(x) ((x) & 0x80000000)
+// pa5
 static inline int F2int(FLOAT a) {
-  if ((a & 0x80000000) == 0) {
-    return a >> 16;
-  }
-  else {
-    return -((-a) >> 16);
+  if(isNeg(a)){
+    return a>>16;
+  }else{
+    return -((-a)>>16);
   }
 }
 
 static inline FLOAT int2F(int a) {
-  if ((a & 0x80000000) == 0) {
-    return a << 16;
+  if(isNeg(a)){
+    return -((-a)<<16);
+  }else{
+    return a<<16;
   }
-  else {
-    return -((-a) << 16);
-  }
+}
+
+static inline FLOAT F_mul_int(FLOAT a, int b) {
+  return a*b;
+}
+
+static inline FLOAT F_div_int(FLOAT a, int b) {
+  return a/b;
 }
 
 FLOAT f2F(float);
 FLOAT F_mul_F(FLOAT, FLOAT);
 FLOAT F_div_F(FLOAT, FLOAT);
-
-static inline FLOAT F_mul_int(FLOAT a, int b) { 
-  return F_mul_F(a, int2F(b));
-}
-
-static inline FLOAT F_div_int(FLOAT a, int b) {
-   return F_div_F(a, int2F(b));
-}
-
 FLOAT Fabs(FLOAT);
 FLOAT Fsqrt(FLOAT);
 FLOAT Fpow(FLOAT, FLOAT);
