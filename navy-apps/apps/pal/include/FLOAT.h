8a53fd14e6739a16336a32109015d8f19ad7b9ea
tracer-ics2017 <tracer@njuics.org>
2023-06-14 02:38:08 +0800

    >  run 2010507 zihan Linux ubuntu 5.15.0-73-generic #80~20.04.1-Ubuntu SMP Wed May 17 14:58:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:38:08 up 12:23,  1 user,  load average: 0.22, 0.20, 0.51 be1449309c4be38a36a81c664acaf684cc25f5b

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -2,38 +2,39 @@
 #define __FLOAT_H__
 
 #include "assert.h"
-
+#include "common.h"
 typedef int FLOAT;
 
-#define isNeg(x) ((x) & 0x80000000)
-// pa5
 static inline int F2int(FLOAT a) {
-  if(isNeg(a)){
-    return a>>16;
-  }else{
-    return -((-a)>>16);
+  if ((a & 0x80000000) == 0) {
+    return a >> 16;
+  }
+  else {
+    return -((-a) >> 16);
   }
 }
 
 static inline FLOAT int2F(int a) {
-  if(isNeg(a)){
-    return -((-a)<<16);
-  }else{
-    return a<<16;
+  if ((a & 0x80000000) == 0) {
+    return a << 16;
   }
-}
-
-static inline FLOAT F_mul_int(FLOAT a, int b) {
-  return a*b;
-}
-
-static inline FLOAT F_div_int(FLOAT a, int b) {
-  return a/b;
+  else {
+    return -((-a) << 16);
+  }
 }
 
 FLOAT f2F(float);
 FLOAT F_mul_F(FLOAT, FLOAT);
 FLOAT F_div_F(FLOAT, FLOAT);
+
+static inline FLOAT F_mul_int(FLOAT a, int b) { 
+  return F_mul_F(a, int2F(b));
+}
+
+static inline FLOAT F_div_int(FLOAT a, int b) {
+   return F_div_F(a, int2F(b));
+}
+
 FLOAT Fabs(FLOAT);
 FLOAT Fsqrt(FLOAT);
 FLOAT Fpow(FLOAT, FLOAT);
--- navy-apps/apps/pal_1/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal_1/src/FLOAT/FLOAT.c
@@ -1,32 +1,41 @@
 #include "FLOAT.h"
 #include <stdint.h>
 #include <assert.h>
-#include <string.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
- // Log("mul:%x::::%x",a,b);
   return (a * b) >> 16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-  FLOAT result = Fabs(a) / Fabs(b);
-  FLOAT m = Fabs(a);
-  FLOAT n = Fabs(b);
-  m = m % n;
+  // return (a/b)<<16;
+  assert(b != 0);
+  FLOAT m_a = Fabs(a);
+  FLOAT m_b = Fabs(b);
+  FLOAT res = m_a / m_b;
+  FLOAT remain = m_a % m_b;
 
   for (int i = 0; i < 16; i++) {
-    m <<= 1;
-    result <<= 1;
-    if (m >= n) {
-      m -= n;
-      result++;
+    remain = remain << 1;
+    res = res << 1;
+    if (remain >= m_b) {
+      remain -= m_b;
+      res++;
     }
   }
   if (((a ^ b) & 0x80000000) == 0x80000000) {
-    result = -result;
+    res = -res;
   }
-  return result;
+  return res;
 }
+
+union float_s {
+    struct {
+      uint32_t m : 23;
+      uint32_t e : 8;
+      uint32_t s : 1;
+    };
+    uint32_t value;
+};
 
 FLOAT f2F(float a) {
   /* You should figure out how to convert `a' into FLOAT without
@@ -37,39 +46,45 @@ FLOAT f2F(float a) {
    * Hint: The bit representation of `a' is already on the
    * stack. How do you retrieve it to another variable without
    * performing arithmetic operations on it directly?
+   * “a”的位表示形式已在堆栈上。如何在不直接对其执行算术运算的情况下将其检索到另一个变量？
    */
-  union float_ {
-    struct {
-      uint32_t m : 23;
-      uint32_t e : 8;
-      uint32_t signal : 1;
-    };
-    uint32_t value;
-  };
-  union float_ f;
+
+  // assert(0);
+  union float_s f;
   f.value = *((uint32_t*)(void*)&a);
-
-  int e = f.e - 127;
-
-  FLOAT result;
-  if (e <= 7) {
-    result = (f.m | (1 << 23)) >> 7 - e;
+  int real_e = f.e - 127;      //127为指数的偏移量！不能忘记减去
+  FLOAT res = 0;
+  if (real_e == 128)
+    assert(0);
+  int mov = 23- real_e - 16;
+  if (mov >= 0)
+    res = (f.m | (1 << 23)) >> mov;
+  else
+    res = (f.m | (1 << 23)) << (-mov);
+  if(f.s!=0){
+    res=-res;
   }
-  else {
-    result = (f.m | (1 << 23)) << (e - 7);
+  return res;
+}
+
+FLOAT Fabs(FLOAT a) {
+  if(isNeg(a)){
+    return -a;
+  }else{
+    return a;
   }
-  return f.signal == 0 ? result : (result|(1<<31));
 }
 
 /* Functions below are already implemented */
 
 FLOAT Fsqrt(FLOAT x) {
   FLOAT dt, t = int2F(2);
-  
+
   do {
     dt = F_div_int((F_div_F(x, t) - t), 2);
     t += dt;
   } while(Fabs(dt) > f2F(1e-4));
+
   return t;
 }
 
@@ -82,5 +97,6 @@ FLOAT Fpow(FLOAT x, FLOAT y) {
     dt = (F_div_F(x, t2) - t) / 3;
     t += dt;
   } while(Fabs(dt) > f2F(1e-4));
+
   return t;
 }
